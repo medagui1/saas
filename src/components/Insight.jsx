442634ef5785aa240950e8a5e191e20e9288468f
Med Agui <simohamedregragui2@gmail.com>
2023-12-19 15:54:42 +0100

    fixing the Insight components

--- src/components/Insight.jsx
+++ src/components/Insight.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Insight = ({ image, content }) => {
   return (
     <div
-      className="lg:min-h-[600px] max-sm:min-h-[400px] w-full bg-cover py-40 px-8 mt-20 max-sm:mt-4 max-sm:p-4 flex justify-center items-center"
+      className="lg:min-h-[600px] max-sm:min-h-[400px] w-full bg-cover py-40 px-16 mt-20 max-sm:mt-4 max-sm:p-4 flex max-sm:justify-center items-center"
       style={{ 
         backgroundImage: `url(${image})`,
         backgroundPositionX: "right" }}
